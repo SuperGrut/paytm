fab6da5c293914a54424fc4ba51a532d4da73113
iAkash <Akashsah946@gmail.com>
2025-07-17 12:40:18 +0545

    i f&*ked up

---
+++ script.js
@@ -0,0 +1,11 @@
+const link = document.getElementById("link");
+
+// //add popover
+// link.addEventListener("mouseenter", () => {
+//   link.children[1].classList.remove("sub-nav-link");
+// });
+
+// //remove popover
+// link.addEventListener("mouseleave", () => {
+//   link.children[1].classList.add("sub-nav-link");
+// });
